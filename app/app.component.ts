2989ee17fc95b2e58cb4b512c27a00a41e6766b4
fernoftheandes <fernoftheandes@gmail.com>
2016-07-06 20:28:35 -0400

    - We created a service class that can be shared by many components. - We used the ngOnInit Lifecycle Hook to get our heroes when our AppComponent activates. - We defined our HeroService as a provider for our AppComponent. - We created mock hero data and imported them into our service. - We designed our service to return a Promise and our component to get our data from the Promise.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -63,30 +65,29 @@ import { HeroDetailComponent } from './hero-detail.component';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 
 })
-export class AppComponent {
-  public heroes = HEROES;
+export class AppComponent implements OnInit {
+  heroes;
 
   title = 'Tour of Heroes';
 
   selectedHero: Hero;
 
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    // this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   }
 }
-
-const HEROES: Array<Hero> = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
